3f51b9267aa4e4ca787fd3f60d9a0678bd2f58d0
j <www.jtaheri1377@gmail.com>
2025-03-14 12:05:28 +0330

    ...

--- Frontend/news/src/app/modules/meeting/components/meetings/meetings.component.ts
+++ Frontend/news/src/app/modules/meeting/components/meetings/meetings.component.ts
@@ -1,50 +1,52 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { MeetingService } from '../../services/meeting.service';
-import { combineLatest, forkJoin, Observable, Subscription } from 'rxjs';
-import { NewsResponse } from '../../../../core/models/News/news.model';
-import { NewsItem } from '../../../../core/models/News/newsItem.model';
+import { NewsCategories } from "./../../../../core/news-categories";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { MeetingService } from "../../services/meeting.service";
+import { combineLatest, forkJoin, Observable, Subscription } from "rxjs";
+import { NewsResponse } from "../../../../core/models/News/news.model";
+import { NewsItem } from "../../../../core/models/News/newsItem.model";
 
 @Component({
-  selector: 'app-meetings',
+  selector: "app-meetings",
   standalone: false,
 
-  templateUrl: './meetings.component.html',
-  styleUrl: './meetings.component.scss',
+  templateUrl: "./meetings.component.html",
+  styleUrl: "./meetings.component.scss",
 })
 export class MeetingsComponent implements OnInit, OnDestroy {
   subs: Subscription[] = [];
   publicItems!: NewsResponse;
   boardItems!: NewsResponse;
   provincialItems!: NewsResponse;
+  newsCategories = NewsCategories;
   isLoading: boolean = false;
   bannerItems = [
     {
       id: 0,
-      img: './test/namayande2.jpg',
-      title: 'دیدار نماینده مجلس خبرگان با رئیس مجمع نمایندگان طلاب شیراز',
-      studyTime: '4',
-      date: 'دو روز پیش',
+      img: "./test/namayande2.jpg",
+      title: "دیدار نماینده مجلس خبرگان با رئیس مجمع نمایندگان طلاب شیراز",
+      studyTime: "4",
+      date: "دو روز پیش",
     },
     {
       id: 1,
-      img: './test/namayande5.jpg',
-      title: 'همایش جهاد تبیین و انتخابات کرمان',
-      studyTime: '5',
-      date: 'در این هفته',
+      img: "./test/namayande5.jpg",
+      title: "همایش جهاد تبیین و انتخابات کرمان",
+      studyTime: "5",
+      date: "در این هفته",
     },
     {
       id: 2,
-      img: './test/namayande3.jpg',
-      title: 'مراسم تجلیل از بزرگان علمی شیراز',
-      studyTime: '7',
-      date: 'در این هفته',
+      img: "./test/namayande3.jpg",
+      title: "مراسم تجلیل از بزرگان علمی شیراز",
+      studyTime: "7",
+      date: "در این هفته",
     },
     {
       id: 3,
-      img: './test/namayande4.jpg',
-      title: 'مراسم عمامه گذاری نیمه شعبان طلاب اصفهان',
-      studyTime: '3',
-      date: 'دیروز',
+      img: "./test/namayande4.jpg",
+      title: "مراسم عمامه گذاری نیمه شعبان طلاب اصفهان",
+      studyTime: "3",
+      date: "دیروز",
     },
   ];
 
